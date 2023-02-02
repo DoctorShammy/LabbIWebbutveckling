f5b1a73d5ea8f473dbf2f1da18f73096175ef5cf
Vera <vera.krapivner@iths.se>
2023-02-02 16:42:25 +0100

    Add project files.

---
+++ LIW.Membership.Database/Enteties/Genre.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIW.Membership.Database.Enteties
+{
+	public class Genre : IEntity
+	{
+		public int Id { get; set; }
+	}
+}
